c0eabb5bbdde2ed24421a4745ddcaba2c14bda46
tylerboy0604 <77289575+Tdvds@users.noreply.github.com>
2023-05-22 11:31:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,6 +27,23 @@
         }
     });
 
+
+
+    $(".scroll-to-bottom").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
     $(".btnsmooth").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
